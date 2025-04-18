410c3cd7b0fe227f7930b0469ce82062117b8d07
RadkovEugene <eugene.radkov@aerlingus.com>
2025-04-18 22:30:42 +0200

    Task 3

---
+++ task3.js
@@ -0,0 +1,13 @@
+let a; 
+let b; 
+
+a = prompt('Enter first number', );
+b = prompt('Enter second number', );
+
+if (a > b){
+    alert(b);
+} else if(a == b) {
+    alert('Numbers equal - ' + a); 
+} else {
+    alert(a);
+}
