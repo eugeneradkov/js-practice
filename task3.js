aee710df080ed7e2813c0554172d60e8fce5f45e
RadkovEugene <eugene.radkov@aerlingus.com>
2025-04-18 22:39:27 +0200

    task 4 correction

--- task3.js
+++ task3.js
@@ -1,13 +1,17 @@
-let a; 
-let b; 
+function min(){
+    let a; 
+    let b; 
 
-a = prompt('Enter first number', );
-b = prompt('Enter second number', );
+    a = prompt('Enter first number', );
+    b = prompt('Enter second number', );
 
-if (a > b){
-    alert(b);
-} else if(a == b) {
-    alert('Numbers equal - ' + a); 
-} else {
-    alert(a);
+    if (a > b){
+        alert(b);
+    } else if(a == b) {
+        alert('Numbers equal - ' + a); 
+    } else {
+        alert(a);
+    }
 }
+
+min();
