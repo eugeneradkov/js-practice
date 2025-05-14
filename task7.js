ef431f2ecec84f2148cd56d0e7c3352ad400cc8e
eugeneradkov <eugene.radkov@gmail.com>
2025-05-14 17:44:03 +0200

    task 7 update

---
+++ task7.js
@@ -0,0 +1,11 @@
+function isItUpperCase(){
+
+    let letter = prompt('Enter letter in any case')
+
+    if (letter == letter.toUpperCase) {
+        console.log('Оууу май, большая буква!')
+    } else {
+        console.log('Нет уж, маленькие буквы - скучно')
+    }
+
+}
